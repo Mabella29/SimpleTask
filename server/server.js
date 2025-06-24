a5c78b0e1708349e939c9470922b7b9168743bae
Mabella29 <nanaarquosuarh@gmail.com>
2025-06-24 01:38:24 +0000

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -13,3 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/tasks',Router)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=> console.log(`Server is running on http://localhost:${PORT}`))
